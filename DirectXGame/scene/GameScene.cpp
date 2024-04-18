473d6f836767b0bc38f76b7051f5a0b5a7e627fa
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-04-18 13:14:11 +0900

    2024/04/18(木)
    sprite、モデル、音声、ImGuiなどを入れた。
    音声のループだと４４行目に参考にて、PlayWave()の引数「true」を入れないと一回しか鳴らないのでご注意
    
    ImGuiのライブラリはデバッガーのための機能なので注意

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,19 +1,60 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "ImGuiManager.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+
+	textureHandle_ = TextureManager::Load("sample.png");
+	sprite_ = Sprite::Create(textureHandle_, { 100, 50 });
+	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
+	
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	model_ = Model::Create();
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	Vector2 position = sprite_->GetPosition();
+
+	position.x += 2.0f;
+	position.y += 1.0f;
+
+	sprite_->SetPosition(position);
+
+	if (input_->TriggerKey(DIK_SPACE)) {
+		if (audio_->IsPlaying(voiceHandle_)) {
+			audio_->StopWave(voiceHandle_);
+		}
+		else {
+			voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+		}
+	}
+
+	ImGui::Begin("PlayerParameter");
+	ImGui::Text("Name : Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::Text("HP : %d", 50);
+	ImGui::Text("Magic : %d", 100);
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+
+	ImGui::ShowDemoWindow();
+}
 
 void GameScene::Draw() {
 
@@ -41,6 +82,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -53,6 +95,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
+
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
