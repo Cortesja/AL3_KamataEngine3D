02cc9a40fd40a09afad1e86f491e352e654b3f23
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-05-20 16:08:44 +0900

    player.cpp更新

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,1 +1,26 @@
 #include "Player.h"
+#include <cassert>
+
+Player::Player()
+{
+}
+
+Player::~Player() {
+	delete model_;
+}
+
+void Player::Initialize(Model* model, uint32_t textureHandler, ViewProjection* viewProjection) {
+	assert(model);
+	model_ = model;
+	textureHandle_ = textureHandler;
+	viewProjection_ = viewProjection;
+	worldTransform_.Initialize();
+}
+
+void Player::Update() {
+	worldTransform_.TransferMatrix();
+}
+
+void Player::Draw() {
+	model_->Draw(worldTransform_, *viewProjection_, textureHandle_);
+}
